63a431a09c0f496d07c53994fd42a63e8e206c72
Roy Reyna <royreyna7@yahoo.com>
2020-04-01 20:14:49 -0500

    switch statements

--- liri.js
+++ liri.js
@@ -1,4 +1,39 @@
 require("dotenv").config();
+var axios = require("axios");
 var keys = require("./keys.js");
-var spotify = new Spotify(keys.spotify);
+//var spotify = new spotify(keys.spotify);
+
+var action = process.argv[2];
+var argument = process.argv[3];
+
+switch(action){
+    case "concert-this":
+        concert(argument);
+        break;
+    case "spotify-this-song":
+        song(argument);
+        break;
+    case "movie-this":
+        movie(argument);
+        break;
+    case "do-what-it-says":
+        read();
+        break;
+}
+
+function concert(band){
+
+}
+
+function song(name){
+
+}
+
+function movie(title){
+
+}
+
+function read(){
+
+}
 
