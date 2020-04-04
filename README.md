acdfdb85116971f3ad1e7b1623316bf90d52d4bb
Roy Reyna <royreyna7@yahoo.com>
2020-04-04 18:55:34 -0500

    comments added

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # liri-node-app
+
+1. The liri-app that I've created is meant to give the user appropriate song, movie, or concert info based on the user's selection.
+
+2.
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//require files and npms for app functionality
 require("dotenv").config();
 
 var fs = require("fs");
@@ -7,9 +8,11 @@ var moment = require("moment");
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
+//grab user input and store as variables to pass to switch statement
 var action = process.argv[2];
 var argument = process.argv.slice(3).join(" ");
 
+//run the appropriate case based from the user's requested action
 switch(action){
     case "concert-this":
         concert(argument);
@@ -25,10 +28,14 @@ switch(action){
         break;
 }
 
+//function for concert info
 function concert(band){
 
+    //send request to Bands in Town API using axios
     axios.get("https://rest.bandsintown.com/artists/" + band + "/events?app_id=codingbootcamp")
     .then(function(response){
+
+        //create an object to store info of next concert
         var nextEvent =
             {
                 venue: "",
@@ -43,7 +50,8 @@ function concert(band){
             };
 
         nextEvent.venue = response.data[0].venue.name;
-
+        
+        //Verify info is stored in the city, region, and country properties of returned object from API call.
         if(response.data[0].venue.city != ""){
             nextEvent.location = response.data[0].venue.city + ", "
         }
@@ -56,10 +64,12 @@ function concert(band){
             nextEvent.location += response.data[0].venue.country;
         }
 
+        //Grab date and format it to MM/DD/YYYY format using moment
         var newDate = moment(response.data[0].datetime);
         
         nextEvent.date = newDate.format("MM/DD/YYYY") + " at " + newDate.format("h:mm A");
         
+        //Print event info in a user friendly output
         nextEvent.printInfo();
 
 
@@ -69,10 +79,14 @@ function concert(band){
     });
 }
 
+//function to run Spotify call for song info
 function song(name){
+
+    //request to Spotify API using the npm called node-spotify-api 
     spotify
         .search({type: 'track', query: name, limit: 1})
         .then(function(response){
+            //Appropriate Song Info is displayed terminal
             console.log("\nArtist: " + response.tracks.items[0].artists[0].name);
             console.log("Song: " + name);
             console.log("Album: " + response.tracks.items[0].album.name +"\n");
@@ -83,10 +97,12 @@ function song(name){
         })
 }
 
+//function to make API call using axios to OMDB API to gather movie info
 function movie(title){
 
     axios.get("http://www.omdbapi.com/?i=tt3896198&apikey=222c6746&t=" + title)
     .then(function(response){
+        //Movie Properties are displayed in terminal
         console.log("\nMovie Info for: " + title +"\n");
         console.log("Title: " + response.data.Title);
         console.log("Year Released: "  +response.data.Year);
@@ -104,6 +120,7 @@ function movie(title){
 }
 
 function read(){
+    //"random.txt" file is read to perform an action instead of grabbing user input
     fs.readFile("random.txt", "utf8", function(error, data) {
 
         if (error) {
